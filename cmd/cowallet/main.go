31bc5a8795aa9b64146e90019b81755ee8f2dfdb
yiplee <guoyinl@gmail.com>
2024-05-27 15:11:43 +0800

    mod: pay usdt

--- cmd/cowallet/main.go
+++ cmd/cowallet/main.go
@@ -31,7 +31,7 @@ func init() {
 	flag.StringVar(&cfg.dbPath, "db", "cowallet.db", "database path")
 	flag.StringVar(&cfg.keystorePath, "key", "key.json", "keystore path")
 	flag.IntVar(&cfg.port, "port", 8080, "http port")
-	flag.StringVar(&cfg.payAsset, "asset", "31d2ea9c-95eb-3355-b65b-ba096853bc18", "pay asset id")
+	flag.StringVar(&cfg.payAsset, "asset", "4d8c508b-91c5-375b-92b0-ee702ed2dac5", "pay asset id")
 	flag.Float64Var(&cfg.payAmount, "amount", 10, "pay amount per month")
 
 	flag.Parse()
